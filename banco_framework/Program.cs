83b6ecbadcbb4f7e718288de5c6e5b3a56f09428
Matheus Salomão <mathesbh@gmail.com>
2023-11-15 14:50:32 -0300

    feat: valida dados input e retorno erros

--- banco_framework/Program.cs
+++ banco_framework/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Application;
+using CpfCnpjLibrary;
 using Domain.Model;
 
 internal class Program
@@ -25,22 +26,40 @@ internal class Program
 
     static Cliente Identificacao()
     {
-        var cliente = new Cliente();
+        var erros = new List<string>();
 
         Console.WriteLine("Seu número de identificação:");
-        cliente.Id = int.Parse(Console.ReadLine());
+        var inputId = int.TryParse(Console.ReadLine(), out var id);
+        if (!inputId)
+            erros.Add("Identificador não é válido");
 
         Console.WriteLine("Seu nome:");
-        cliente.Nome = Console.ReadLine();
+        var inputNome = Console.ReadLine();
+        if (string.IsNullOrEmpty(inputNome) || inputNome.Length <= 3)
+            erros.Add("Nome digitado não é válido");
 
         Console.WriteLine("Seu CPF:");
-        cliente.Cpf = Console.ReadLine();
+        var inputCPF = Console.ReadLine();
+        var validaCPF = Cpf.Validar(inputCPF);
+        if (!validaCPF)
+            erros.Add("CPF digitado não é válido");
 
         Console.WriteLine("Seu saldo:");
-        cliente.Saldo = double.Parse(Console.ReadLine());
+        var inputSaldo = double.TryParse(Console.ReadLine(),out var saldo);
+        if(!inputSaldo || saldo <= 0)
+            erros.Add("Saldo não é válido");
+
+        if(erros.Any())
+        {
+            erros.ForEach(x => Console.WriteLine(x));
+            Console.ReadKey();
+            Console.Clear();
+            Identificacao();
+        }
 
         Console.Clear();
 
+        var cliente = new Cliente(id, inputNome, inputCPF, saldo);
         Console.WriteLine($"Como posso ajudar {cliente.Nome}?");
 
         return cliente;
